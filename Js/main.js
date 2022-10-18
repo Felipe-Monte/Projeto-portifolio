69032fa931ad8a0b51061493717b4e955f10bc9c
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2022-10-17 21:08:23 -0300

    Update js and css

--- Js/main.js
+++ Js/main.js
@@ -1,24 +1,29 @@
 var btnContact = document.querySelector('.icon-contact');
 var closeItens = document.querySelectorAll('.close-itens')
 
-// Sumir preLoader enquanto carrega a página.
+// force page up.
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+// fade preloader while loading the page.
 window.addEventListener('load', function () {
     var preLoader = document.querySelector('.preloader');
     preLoader.classList.add('opacity-auto')
-
+    
     setTimeout(function () {
         preLoader.style.display = 'none'
     }, 2000)
 })
 
-// Aparecer e sumir com a box de contato quando clicar no icone.
+// Appear and disappear with the contact box when you click the icon.
 btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.box-contact');
     boxContact.classList.toggle('is-open');
     this.classList.toggle('icon-contact-move')
 });
 
-//Aparecer e fechar com modal e overlay ao clicar, e add anim.
+// Appear and close with modal and overlay when clicked, and add animation.
 for(var i = 0; i < closeItens.length; i++){
     closeItens[i].addEventListener('click', function(){
         var overlay = document.querySelector('.overlay')
@@ -29,5 +34,17 @@ for(var i = 0; i < closeItens.length; i++){
         modal.classList.toggle('animation-modal')
     })
 }
+
+// appear with the down arrow when you scroll the page.
+window.addEventListener('scroll', function(){
+    var arrowDown = document.querySelector('.slide-bottom')
+    if (window.scrollY > 500 ) {
+        arrowDown.classList.add('opacity-new')
+    } else {
+        arrowDown.classList.remove('opacity-new')
+    }
+
+})
+
 
 
